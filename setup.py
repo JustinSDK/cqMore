6fec67361075a1f059ee46f4617787d3c6e4c26b
Justin Lin <justin.sdk@gmail.com>
2021-10-07 15:36:11 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cqMore',
+    version = '0.0.0',
+    description = 'cadquery plugin (under construction)',
+    author = 'Justin Lin',
+    author_email = 'caterpillar@openhome.cc',
+    license = 'Apache License 2.0',
+    url = 'https://github.com/JustinSDK/cqMore',
+    packages = find_packages()
+)
