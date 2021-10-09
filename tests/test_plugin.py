68d788e2cc0c02edcc8bedea7b54d289bdc80661
Justin Lin <justin.sdk@gmail.com>
2021-10-09 15:59:33 +0800

    add test_hull2D

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -75,6 +75,22 @@ class TestWorkplane2D(unittest.TestCase):
         actual = cast(Wire, r1.cut2D(wire).val())
         self.assertWireEqual(expected, actual)       
 
+    def test_hull2D(self):
+        points = [
+            (0.855355763576732, 0.16864474737612778), 
+            (0.7653639827409133, 0.21243642222244463), 
+            (0.9956528164357294, 0.6119951986040002), 
+            (0.34997550432063895, 0.1878314178942282), 
+            (0.5811634308956635, 0.8646520280492559), 
+            (0.4556958318174945, 0.16723661322362438), 
+            (0.9398877210188867, 0.2413165664583884), 
+            (0.375900956434601, 0.09820967766941846), 
+            (0.8532633331060003, 0.5955415267257996)
+        ]
+
+        pts = Workplane().hull2D(points, forConstruction = True).vertices().vals()
+        self.assertEqual(6, len(pts))
+
     def assertWireEqual(self, expected, actual):
         self.assertEqual(expected.geomType(), actual.geomType())
         self.assertEqual(expected.Center(), actual.Center())
