0e56516c1d360d09e6c0344929fff9337e820d6a
Justin Lin <justin.sdk@gmail.com>
2021-10-11 15:31:03 +0800

    update docstrings

--- cqMore/plugin.py
+++ cqMore/plugin.py
@@ -294,11 +294,11 @@ class Workplane(cadquery.Workplane):
             from cqmore import Workplane
 
             polyline = (Workplane()
-                        .polylineJoin(
-                            [(0, 0, 0), (10, 0, 0), (10, 0, 10), (10, 10, 10)], 
-                            Workplane().box(1, 1, 1)
+                            .polylineJoin(
+                                [(0, 0, 0), (10, 0, 0), (10, 0, 10), (10, 10, 10)], 
+                                Workplane().box(1, 1, 1)
                             )
-                    )
+                        )
 
         """       
 
--- test.py
+++
@@ -1,8 +0,0 @@
-from cqmore import Workplane
-
-polyline = (Workplane()
-               .polylineJoin(
-                   [(0, 0, 0), (10, 0, 0), (10, 0, 10), (10, 10, 10)], 
-                   Workplane().box(1, 1, 1)
-                )
-           )
