ed56a1d1bb688a845d592463d9a48f6352960b6b
Justin Lin <justin.sdk@gmail.com>
2021-10-11 16:23:24 +0800

    add test

---
+++ tests/test_polyhedron.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from cqmore.polyhedron import uvSphere
+from cqmore.cq_typing import FaceIndices
+from typing import cast
+
+class TestPolyhedron(unittest.TestCase):
+    def test_uvSphere(self):
+        sphere = uvSphere(10, 3)
+        self.assertListEqual(
+            [(0, 1, 7), (0, 7, 6), (1, 2, 8), (1, 8, 7), (2, 3, 9), (2, 9, 8), (3, 4, 10), (3, 10, 9), (4, 5, 11), (4, 11, 10), (5, 0, 6), (5, 6, 11), (12, 1, 0), (12, 2, 1), (12, 3, 2), (12, 4, 3), (12, 5, 4), (12, 0, 5), (13, 6, 7), (13, 7, 8), (13, 8, 9), (13, 9, 10), (13, 10, 11), (13, 11, 6)],
+            cast(list[FaceIndices], sphere.faces)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
