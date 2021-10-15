be191c1246cc24b7411f397b48198edef5f23994
Justin Lin <justin.sdk@gmail.com>
2021-10-15 17:39:15 +0800

    add twisted_strips

--- examples/bool2d.py
+++
@@ -1,9 +0,0 @@
-from cqmore import Workplane
-
-r1 = Workplane('YZ').rect(10, 10)
-r2 = Workplane('YZ').center(5, 5).rect(10, 10)
-r3 = Workplane('YZ').center(-2, -2).rect(10, 10)
-r4 = r1.intersect2D(r2).intersect2D(r3)
-
-# show_object([r1, r2, r3])
-# show_object(r4.extrude(1))
---
+++ examples/twisted_strips.py
@@ -0,0 +1,21 @@
+from math import sin, cos, radians
+from cqmore import Workplane
+
+u_step = 10
+v_step = 0.2
+thickness = 0.1
+
+points = []
+for u in range(0, 720 + u_step, u_step):
+    row = []
+    for vi in range(5):
+        v = -1 + vi * v_step
+        row.append((
+            (1 + v / 2 * cos(radians(u / 2))) * cos(radians(u)), 
+            (1 + v / 2 * cos(radians(u / 2))) * sin(radians(u)), 
+            v / 2 * sin(radians(u / 2))
+        ))
+    
+    points.append(row)
+
+twisted_strips = Workplane().gridSurface(points, thickness)
