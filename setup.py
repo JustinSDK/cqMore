a4fd06df438ec04fd0c02b30c84f360792589492
Justin Lin <justin.sdk@gmail.com>
2021-10-17 18:25:33 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'cqMore',
+    name = 'cqmore',
     version = '0.0.0',
     description = 'cadquery plugin (under construction)',
     author = 'Justin Lin',
