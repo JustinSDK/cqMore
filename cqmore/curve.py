210f418b2857593d4bd9a1edd2eec639da15023b
Justin Lin <justin.sdk@gmail.com>
2021-10-23 15:05:20 +0800

    add type hint

--- cqmore/curve.py
+++ cqmore/curve.py
@@ -1,23 +1,24 @@
 from math import sin, cos, tau
+from typing import Any, Callable, Protocol, Union
 
-from .cq_typing import Point2D
+from .cq_typing import Point2D, Point3D
 
 def circle(t: float, radius: float) -> Point2D:
     theta = t * tau
     return (radius * cos(theta), radius * sin(theta))
 
 
-def helix(t, radius, slope):
+def helix(t: float, radius: float, slope: float) -> Point3D:
     theta = t * tau
     return (radius * cos(theta), radius * sin(theta), radius * slope * t)
 
 
-def torusKnot(t, p, q):
+def torusKnot(t: float, p: int, q: int) -> Point3D:
     phi = t * tau
     r = cos(q * phi) + 2
     p_phi = p * phi
     return (r * cos(p_phi), r * sin(p_phi), -sin(q * phi))
 
 
-def parametricEquation(f, **params):
-    return lambda t: f(t, **params)
+def parametricEquation(f: Callable[..., Union[Point2D, Point3D]], *args: Any, **kwargs: Any) -> Callable[[float], Union[Point2D, Point3D]]:
+    return lambda t: f(t, *args, **kwargs)
