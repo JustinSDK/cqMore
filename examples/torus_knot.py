08c7aaf3ed1edead497c86c4f31f90d363cc0ce1
Justin Lin <justin.sdk@gmail.com>
2021-10-24 17:39:04 +0800

    use star polygon

--- examples/torus_knot.py
+++ examples/torus_knot.py
@@ -1,5 +1,6 @@
 from cqmore import Workplane
 from cqmore.curve import torusKnot, parametricEquation
+from cqmore.polygon import star
 
 from cadquery import Plane, Vector
 
@@ -8,10 +9,11 @@ v1 = Vector(*torusKnot(0.9, p = 2, q = 3))
 v2 = Vector(*torusKnot(0.1, p = 2, q = 3))
 
 r = (Workplane(Plane(origin = origin, normal=(v2 - v1)))
-        .polygon(5, 1)
+        .makePolygon([(p[0] * 0.75, p[1] * 0.75) for p in star()])
         .sweep(
             Workplane().parametricCurve(
-                parametricEquation(torusKnot, p = 2, q = 3)
+                parametricEquation(torusKnot, p = 2, q = 3),
+                N = 96
             ), 
             auxSpine = Workplane('XZ').rect(1, 1)
         )
