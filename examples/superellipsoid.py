68ea2d7a573bed7cfa2e099056b8423cc67ad80b
Justin Lin <justin.sdk@gmail.com>
2021-10-27 15:50:15 +0800

    update examples

--- examples/superellipsoid.py
+++ examples/superellipsoid.py
@@ -1,4 +1,13 @@
 from cqmore import Workplane
 from cqmore.polyhedron import superellipsoid
 
-r = Workplane().polyhedron(*superellipsoid(0.5, 2.8))
+step = 0.5
+cols = 8
+rows = 6
+
+superellipsoids = Workplane()
+for ei in range(1, cols):
+    e = ei * step
+    for ni in range(1, rows):
+        n = ni * step
+        superellipsoids.add(Workplane().polyhedron(*superellipsoid(e, n)).translate((ei * 2.5, ni * 2.5, 0)))
