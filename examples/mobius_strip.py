1a351fd40592ccbb37a1c78b0d57443a0c4868f8
Justin Lin <justin.sdk@gmail.com>
2021-10-31 19:52:34 +0800

    add frags param

--- examples/mobius_strip.py
+++ examples/mobius_strip.py
@@ -7,9 +7,10 @@ profile = [(10, -1, 0), (10, 1, 0), (-10, 1, 0), (-10, -1, 0)]
 translationX20 = translation((20, 0, 0))
 rotationX90 = rotationX(90)
 
-angle_step = 15
+frags = 24
+angle_step = 360 / frags
 profiles = []
-for i in range(24):
+for i in range(frags):
     m = rotationZ(i * angle_step) * translationX20 * rotationX90 * rotationZ(i * angle_step / 2)
     profiles.append([m.transform(p) for p in profile])
 
