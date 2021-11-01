e60ee220f7efa0d82868ae3522c99c0e357f56c6
Justin Lin <justin.sdk@gmail.com>
2021-11-01 17:46:48 +0800

    add example

---
+++ examples/gyroid.py
@@ -0,0 +1,40 @@
+import numpy as np
+from skimage import measure
+from math import sin, cos, radians
+from cqmore import Workplane
+
+def gyroid(x, y, z, thickness, end):
+    # is boundary?
+    if x == 0 or y == 0 or z == 0 or x == end or y == end or z == end:
+        return 0
+
+    rx = radians(x)
+    ry = radians(y)
+    rz = radians(z)
+    v = sin(rx) * cos(ry) + sin(ry) * cos(rz) + sin(rz) * cos(rx)
+    return 1 if -thickness < v < thickness else 0
+vectorized_gyroid = np.frompyfunc(gyroid, 5, 1)
+
+def is_triangle(points, f):
+    s = set([tuple(points[i]) for i in f])
+    return len(s) == 3
+
+
+width = 360
+step = 10
+thickness = 0.2
+
+end = width - step
+arange = np.arange(0, width, step)
+x, y, z = np.meshgrid(arange, arange, arange)
+
+points, faces, _, __ = measure.marching_cubes( # type: ignore
+    vectorized_gyroid(x, y, z, thickness, end), 
+    0, 
+    allow_degenerate = False
+)
+
+g = Workplane().polyhedron(points, faces)
+
+# from cadquery import exporters
+# exporters.export(g, 'gyroid.stl')
