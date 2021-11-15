3bbaa050a8eb2bd76703f71566f6ce29507b8fc5
Justin Lin <justin.sdk@gmail.com>
2021-11-15 17:50:26 +0800

    refactor

--- examples/gyroid.py
+++ examples/gyroid.py
@@ -6,7 +6,7 @@ from math import sin, cos, radians
 from cqmore import Workplane
 
 def gyroid(length, width, height, thickness, step):
-    def _gyroid(x, y, z, thickness, w_end):
+    def _gyroid(x, y, z, thickness, l_end, w_end, h_end):
         # is boundary?
         if x == 0 or y == 0 or z == 0 or x == l_end or y == w_end or z == h_end:
             return 0
@@ -18,7 +18,7 @@ def gyroid(length, width, height, thickness, step):
         rz = radians(z)
         v = sin(rx) * cos(ry) + sin(ry) * cos(rz) + sin(rz) * cos(rx)
         return 1 if -half_thickness < v < half_thickness else 0
-    vectorized_gyroid = np.frompyfunc(_gyroid, 5, 1)
+    vectorized_gyroid = np.frompyfunc(_gyroid, 7, 1)
 
     l_end = length - step
     w_end = width - step
@@ -29,12 +29,12 @@ def gyroid(length, width, height, thickness, step):
     x, y, z = np.meshgrid(l_arange, w_arange, h_arange)
 
     points, faces, _, __ = measure.marching_cubes( # type: ignore
-        vectorized_gyroid(x, y, z, thickness, w_end), 
+        vectorized_gyroid(x, y, z, thickness, l_end, w_end, h_end), 
         0, 
         allow_degenerate = False
     )
 
-    return Workplane().polyhedron(points, faces)
+    return Workplane().polyhedron(points * 10, faces)
 
 length = 360
 width = 360
