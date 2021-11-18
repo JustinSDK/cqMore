99fa5223555673dded507f96677aed59d4c63d43
Justin Lin <justin.sdk@gmail.com>
2021-11-18 16:09:49 +0800

    fix radian error

--- examples/gyroid.py
+++ examples/gyroid.py
@@ -5,10 +5,10 @@ from skimage import measure
 from math import sin, cos, radians
 from cqmore import Workplane
 
-def gyroid(length, width, height, thickness):
-    def _gyroid(x, y, z, thickness, l_end, w_end, h_end):
+def gyroid(length, width, height, thickness, step):
+    def _gyroid(x, y, z, thickness, length, width, height):
         # is boundary?
-        if x == 0 or y == 0 or z == 0 or x == l_end or y == w_end or z == h_end:
+        if x == 0 or y == 0 or z == 0 or x == length or y == width or z == height:
             return 0
 
         half_thickness = thickness / 2
@@ -20,31 +20,30 @@ def gyroid(length, width, height, thickness):
         return 1 if -half_thickness <= v <= half_thickness else 0
     vectorized_gyroid = np.frompyfunc(_gyroid, 7, 1)
 
-    step = thickness * 25
-    l_end = length - step
-    w_end = width - step
-    h_end = height - step
-    l_arange = np.arange(0, length, step)
-    w_arange = np.arange(0, width, step)
-    h_arange = np.arange(0, height, step)
+    
+    l_arange = np.arange(0, length + step, step)
+    w_arange = np.arange(0, width + step, step)
+    h_arange = np.arange(0, height + step, step)
     x, y, z = np.meshgrid(l_arange, w_arange, h_arange)
 
     points, faces, _, __ = measure.marching_cubes( # type: ignore
-        vectorized_gyroid(x, y, z, thickness, l_end, w_end, h_end), 
+        vectorized_gyroid(x, y, z, thickness, length, width, height), 
         0, 
-        spacing = (step, step, step),
+        spacing = (radians(step),) * 3,
         allow_degenerate = False
     )
 
     return Workplane().polyhedron(points, faces)
+
 
 if __name__ == '__main__':
     length = 360
     width = 360
     height = 360
     thickness = 0.4
+    step = 10
 
-    g = gyroid(length, width, height, thickness)
+    g = gyroid(length, width, height, thickness, step)
 
     # from cadquery import exporters
     # exporters.export(g, 'gyroid.stl')
--- examples/gyroid_sphere.py
+++ examples/gyroid_sphere.py
@@ -1,39 +1,45 @@
 # scikit-image 0.18 or later is required.
 
+from math import radians
 from gyroid import gyroid
 from cqmore import Workplane
 from cqmore.polyhedron import uvSphere
 
-def gyroid_sphere(thickness, period):
-    step = thickness * 25
-    length = 360 + step
-    width = 360 + step
-    height = 360 + step
+def gyroid_sphere(thickness, period, step):
+    width = 360 + step * 5
 
-    g = gyroid(length, width, height, thickness)
+    g = gyroid(width, width, width, thickness, step)
+    
+    r = radians(180 * period)
 
-    offset = 360 - step
+    if period == 1:
+        return Workplane().polyhedron(*uvSphere(r, 48, 24)).translate((r, r, r)).intersect(g)
+
+    offset = radians(360)
     rg = range(period)
 
     row = Workplane()
     for i in rg:
         row.add(g.translate((offset * i, 0, 0)))
+    row = row.combine()
 
     rect = Workplane()
     for i in rg:
         rect.add(row.translate((0, offset * i, 0)))
+    rect = rect.combine()
 
     cube = Workplane()
     for i in rg:
         cube.add(rect.translate((0, 0, offset * i)))
+    cube = cube.combine()
 
-    r = 180 * period
     return Workplane().polyhedron(*uvSphere(r, 48, 24)).translate((r, r, r)).intersect(cube)
 
 
 thickness = 0.4
-period = 2
-g = gyroid_sphere(thickness, period)
+period = 4
+step = 10
+g = gyroid_sphere(thickness, period, step)
 
-# from cadquery import exporters
-# exporters.export(g, 'gyroid_sphere.stl')
+from cadquery import exporters
+exporters.export(g, 'gyroid_sphere.stl')
