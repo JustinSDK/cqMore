6ece2bf1d0c6df63eff9d2d451912065204dfa2b
Justin Lin <justin.sdk@gmail.com>
2022-01-17 10:18:46 +0800

    cut amorphous

--- examples/reaction-diffusion.py
+++ examples/reaction-diffusion.py
@@ -3,6 +3,7 @@
 import numpy as np
 from skimage import measure
 from cqmore import Workplane
+from cadquery import exporters
 
 def gray_scott(feel, kill, generation, space_size = 200, init_size = 20, dx = 0.01, dt = 1):
     Du = 2e-5
@@ -39,20 +40,32 @@ feel, kill = 0.04, 0.06      # amorphous
 # feel, kill = 0.035, 0.065  # spots
 # feel, kill = 0.012, 0.05   # wandering bubbles
 # feel, kill = 0.025, 0.05   # waves
-generation = 2000
+generation = 1000
 density_threshold = .5
-layer_h = 1
+layer_h = 2
+line_w = 1.5
+space_size = 200
 
 contours = measure.find_contours(
-    gray_scott(feel, kill, generation), 
+    gray_scott(feel, kill, generation, space_size), 
     density_threshold
 )
 
 all = Workplane()
-for i in range(len(contours)):
-    xs = contours[i][:, 1]
-    ys = contours[i][:, 0]
+for contour in contours:
+    xs = contour[:, 1]
+    ys = contour[:, 0]
     coords = [tuple(coord) for coord in np.dstack([xs, ys])[0]]
-    all.add(Workplane().polyline(coords).close().extrude((i + 1) * layer_h))
+    scope = Workplane().polyline(coords).close()
+    offset = Workplane().polyline(coords).close().offset2D(-line_w)
+    all.add(scope.cut2D(offset).extrude(layer_h * 2))
 
 all = all.combine()
+output = (Workplane().rect(space_size, space_size).extrude(layer_h)
+                     .translate((space_size / 2, space_size / 2, -layer_h / 2))
+                     .cut(all))
+
+exporters.export(
+    output, 
+    'reaction-diffusion.stl'
+)
