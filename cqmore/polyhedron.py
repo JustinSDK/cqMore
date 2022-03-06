2dbfb349e49f0a45ca35857f1eab2ceb2408d228
Justin Lin <justin.sdk@gmail.com>
2022-03-06 16:06:49 +0800

    refactor

--- cqmore/polyhedron.py
+++ cqmore/polyhedron.py
@@ -757,13 +757,9 @@ def superellipsoid(e: float, n: float, widthSegments: int = 3, heightSegments: i
 
     """
 
-    def _sgn(x):
-        if x < 0:
-            return -1
-        elif x == 0:
-            return 0
-        else:
-            return 1
+    # Signum function
+    def _sgn(n):
+        return  n and (1, -1)[n < 0]
     
     def _c(w, m):
         cosw = cos(w)
