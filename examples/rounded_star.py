05d13697803d6da08aa9392b7df8d8dfd0c67813
Justin Lin <justin.sdk@gmail.com>
2022-06-23 10:46:02 +0800

    refactor

--- examples/rounded_star.py
+++ examples/rounded_star.py
@@ -3,15 +3,17 @@ from cqmore.polygon import star
 from cadquery import Edge, Vector, Wire
 
 def roundedStar(outerRadius: float = 1, innerRadius: float = 0.381966, n: int = 5, rounded: float = 0.5, spoke: float = 0.381966) -> Wire:
-    pts = list(star(outerRadius, innerRadius, n))
+    vts = [Vector(*p) for p in star(outerRadius, innerRadius, n)]
     ts = rounded / outerRadius * 3 # tangent scale
     spokes = [1, spoke]
     tangents = [
-        Vector(-ts * pts[i][1] / spokes[i % 2], ts * pts[i][0] / spokes[i % 2], 0)
-        for i in range(len(pts))
+        Vector(-ts * vts[i].y / spokes[i % 2], ts * vts[i].x / spokes[i % 2], 0)
+        for i in range(len(vts))
     ]
 
-    return Wire.assembleEdges([Edge.makeSpline(listOfVector = [Vector(*p) for p in pts], tangents = tangents, periodic = True, scale = False)])
+    return Wire.assembleEdges(
+        [Edge.makeSpline(listOfVector = vts, tangents = tangents, periodic = True, scale = False)]
+    )
 
 outerRadius = 1
 innerRadius = 0.381966
@@ -21,6 +23,4 @@ spoke = 0.381966
 
 w = (Workplane()
         .add(roundedStar(outerRadius, innerRadius, n, rounded, spoke))
-        .toPending()
-        .extrude(1)
     )
