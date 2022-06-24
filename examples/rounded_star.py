a490603a18e87e65d9a8c5d52ea0b0ce402d261c
Justin Lin <justin.sdk@gmail.com>
2022-06-24 22:47:38 +0800

    del spoke param

--- examples/rounded_star.py
+++ examples/rounded_star.py
@@ -2,10 +2,10 @@ from cqmore import Workplane
 from cqmore.polygon import star
 from cadquery import Edge, Vector, Wire
 
-def roundedStar(outerRadius: float = 1, innerRadius: float = 0.381966, n: int = 5, rounded: float = 0.5, spoke: float = 0.381966) -> Wire:
+def roundedStar(outerRadius: float = 1, innerRadius: float = 0.381966, n: int = 5, rounded: float = 0.5) -> Wire:
     vts = [Vector(*p) for p in star(outerRadius, innerRadius, n)]
     ts = rounded / outerRadius * 3 # tangent scale
-    spokes = [1, spoke]
+    spokes = [1, innerRadius / outerRadius]
     tangents = [
         Vector(-ts * vts[i].y / spokes[i % 2], ts * vts[i].x / spokes[i % 2], 0)
         for i in range(len(vts))
@@ -19,8 +19,7 @@ outerRadius = 1
 innerRadius = 0.381966
 n = 5
 rounded = 0.5
-spoke = 0.381966
 
 w = (Workplane()
-        .add(roundedStar(outerRadius, innerRadius, n, rounded, spoke))
+        .add(roundedStar(outerRadius, innerRadius, n, rounded))
     )
